d532b0729d6c1e359a0abe878d008e94430b978f
danillucky1234 <danilyurzhenko2607@gmail.com>
2021-05-25 22:32:59 +0300

    Now you don't have to specify the display number, the display gets from environment variable

--- screencast.sh
+++ screencast.sh
@@ -6,7 +6,8 @@ widthAndHeight=${xrectselOutput%+*} # retain the part before the '+'
 
 corners=${xrectselOutput:${#widthAndHeight}+1} # cut the first part of the string in variable
 
-outputName="$HOME/Videos/" # concatenation of strings to get the output path
+#homePath=$HOME
+outputName="$HOME/Videos/"
 outputName+=$(date +"%Y_%m_%d_%H_%M_%S.mp4")
 
-ffmpeg -video_size $widthAndHeight -framerate 60 -f x11grab -i :1.0+$corners $outputName
+ffmpeg -video_size $widthAndHeight -framerate 60 -f x11grab -i $DISPLAY.0+$corners $outputName
